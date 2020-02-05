bce63f5c9c855f64f82a6705bdd6e56527385cde
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-02-05 21:46:22 +0100

    Added MainMenu

--- Unit1.h
+++ Unit1.h
@@ -9,11 +9,22 @@
 #include <Forms.hpp>
 #include <ExtCtrls.hpp>
 #include <Graphics.hpp>
+#include <Menus.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
         TImage *tlo;
+        TMainMenu *MainMenu1;
+        TMenuItem *Opcje1;
+        TMenuItem *Opis1;
+        TMenuItem *Zapiszgre1;
+        TMenuItem *N1;
+        TMenuItem *Zakocz1;
+        TMenuItem *Pomoc1;
+        TMenuItem *Informacje1;
+        TMenuItem *Oprogramie1;
+        TMenuItem *Autor1;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
